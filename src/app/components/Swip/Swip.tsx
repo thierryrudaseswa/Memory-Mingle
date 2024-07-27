aea6870276ef8e9b54064c32b5dd3397e570afed
thierryrudaseswa <trudaseswa@gmail.com>
2024-07-27 20:35:57 +0200

    The mingle with additional of the fun fact questions and answers

--- src/app/components/Swip/Swip.tsx
+++ src/app/components/Swip/Swip.tsx
@@ -10,20 +10,76 @@ import 'swiper/css/navigation';
 import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 import Image from 'next/image';
+import './Swip.css'
 
-// Import your images
-import slide_image_1 from '../../../../public/Image/thierry.jpg'
-import slide_image_2 from  '../../../../public/Image/thierry.jpg'
-import slide_image_3 from '../../../../public/Image/thierry.jpg'
-import slide_image_4 from '../../../../public/Image/thierry.jpg'
-import slide_image_5 from '../../../../public/Image/thierry.jpg'
-import slide_image_6 from '../../../../public/Image/thierry.jpg'
-import slide_image_7 from '../../../../public/Image/thierry.jpg'
+
+
+import { WishPerson,Person } from '@/app/data/wish';
+
+
+const Card = ({ person }: { person: Person }) => (
+  
+    <div className="smsfr grid  p-3 rounded-3xl  bg-orange-700">
+<div className="  grid bg-white   grid-cols-3">
+<div className=" p-2 h-96 rounded-2xl col-span-1 grid justify-start">
+        <div className="`relative rounded-3xl overflow-hidden p-2 ">
+          {person.photo && (
+            <Image
+              src={person.photo}
+              alt={person.name}
+              layout="fill"
+              className="elem "
+             
+            />
+          )}
+        </div>
+      </div> 
+   
+      <div className="wishes col-span-2 grid justify-start items-center p-2">
+        <div className=" rounded-2xl ">
+          {person.wish && <p className="flex justify-center items-center">{person.wish}</p>}
+          {person.name && <p className="font-extrabold flex justify-end">{person.name}</p>}
+        </div>
+      </div>
+
+</div>
+<div className="">
+<div className="gif  gap-4 p-4 rounded-2xl">
+        <div className=" grid grid-rows-4">
+          {person.promise && <p className="row-span-1 flex justify-center items-center font-bold">{person.promise}</p>}
+          {person.gift && (
+            <p className="font-extrabold grid  pl-2 text-2xl row-span-1 justify-center items-center">
+              {person.gift} !!
+            </p>
+          )}
+          <div className="flex items-center justify-center  row-span-2">
+            <div className="relative rounded-3xl overflow-hidden ">
+              {person.photoTwo && (
+                <Image
+                  src={person.photoTwo}
+                  alt={person.name}
+                  layout="fill"
+                  className="lemen"
+                  sizes="(max-width: 768px) 80vw, (max-width: 1200px) 50vw, 15vw"
+                />
+              )}
+            </div>
+          </div>
+        </div>
+       </div> 
+</div>
+
+
+     
+     
+      
+    </div>
+);
 
 const HomePage: NextPage = () => {
   return (
     <div className="container">
-      <h1 className="heading">Flower Gallery</h1>
+      <div className=" flex justify-center items-start bg-slate-500 h-20">THE MERVEROUS WISHES AND PROMISES</div>
       <Swiper
         modules={[EffectCoverflow, Pagination, Navigation]}
         effect="coverflow"
@@ -35,7 +91,7 @@ const HomePage: NextPage = () => {
           rotate: 0,
           stretch: 0,
           depth: 100,
-          modifier: 2.5,
+          modifier: 12,
         }}
         pagination={{ clickable: true }}
         navigation={{
@@ -44,7 +100,8 @@ const HomePage: NextPage = () => {
         }}
         className="swiper_container"
       >
-        <SwiperSlide>
+        {/* {WishPers} */}
+        {/* <SwiperSlide>
           <Image src={slide_image_1} alt="slide_image" layout="fill" objectFit="cover" />
         </SwiperSlide>
         <SwiperSlide>
@@ -61,10 +118,18 @@ const HomePage: NextPage = () => {
         </SwiperSlide>
         <SwiperSlide>
           <Image src={slide_image_6} alt="slide_image" layout="fill" objectFit="cover" />
+        </SwiperSlide> */}
+
+        {WishPerson.map((person: Person,index:number)=>(
+
+        
+        <SwiperSlide key={index} className="">
+          <Card person={person} />
+
+    
+      
         </SwiperSlide>
-        <SwiperSlide>
-          <Image src={slide_image_7} alt="slide_image" layout="fill" objectFit="cover" />
-        </SwiperSlide>
+        ))}
 
         <div className="slider-controler">
           <div className="swiper-button-prev slider-arrow">
--- src/app/data/wish.tsx
+++ src/app/data/wish.tsx
@@ -17,5 +17,45 @@ export const WishPerson: Person[] = [
         gift: "Lamborghini",
         photoTwo: "/Image/thierry28.jpeg"
     },
+    {
+        name: "Thierry",
+        photo: "/Image/thierry28.jpeg",
+        wish: "Glad that you fine , and allow me to take this moment to wish u happy Birthday be blessed up and more money and vibes",
+        promise: "Excited for your birthday! I have a special gift for you that I know you'll love, which isss",
+        gift: "Lamborghini",
+        photoTwo: "/Image/thierry28.jpeg"
+    },
+    {
+        name: "Thierry",
+        photo: "/Image/thierry28.jpeg",
+        wish: "Glad that you fine , and allow me to take this moment to wish u happy Birthday be blessed up and more money and vibes",
+        promise: "Excited for your birthday! I have a special gift for you that I know you'll love, which isss",
+        gift: "Lamborghini",
+        photoTwo: "/Image/thierry28.jpeg"
+    },
+    {
+        name: "Thierry",
+        photo: "/Image/thierry28.jpeg",
+        wish: "Glad that you fine , and allow me to take this moment to wish u happy Birthday be blessed up and more money and vibes",
+        promise: "Excited for your birthday! I have a special gift for you that I know you'll love, which isss",
+        gift: "Lamborghini",
+        photoTwo: "/Image/thierry28.jpeg"
+    },
+    {
+        name: "Thierry",
+        photo: "/Image/thierry28.jpeg",
+        wish: "Glad that you fine , and allow me to take this moment to wish u happy Birthday be blessed up and more money and vibes",
+        promise: "Excited for your birthday! I have a special gift for you that I know you'll love, which isss",
+        gift: "Lamborghini",
+        photoTwo: "/Image/thierry28.jpeg"
+    },
+    {
+        name: "Thierry",
+        photo: "/Image/thierry28.jpeg",
+        wish: "Glad that you fine , and allow me to take this moment to wish u happy Birthday be blessed up and more money and vibes",
+        promise: "Excited for your birthday! I have a special gift for you that I know you'll love, which isss",
+        gift: "Lamborghini",
+        photoTwo: "/Image/thierry28.jpeg"
+    },
    
 ];
